3f194c34abee93da9008a84e18f61af4f97647e7
Vladimir <casim.vladimir@gmail.com>
2017-11-03 14:18:51 +0200

    Added examples

--- src/main/java/exercises/MaxDuplicateWordCount.java
+++ src/main/java/exercises/MaxDuplicateWordCount.java
@@ -4,7 +4,9 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Stream;
 
@@ -25,22 +27,27 @@ public class MaxDuplicateWordCount {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        oldWayMethod();
     }
 
     public static void oldWayMethod() {
         File file = new File(PATH + "ex_17_04/Exercise_17_04.txt");
+        Map<String, Integer> wordMap = new HashMap<>();
         try (FileInputStream fileInputStream = new FileInputStream(file);
              BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream))) {
-            Set<String> uniqueWords = new HashSet<>();
             String line = bufferedReader.readLine();
             while (line != null) {
                 String[] words = line.split(" ");
                 for (String word : words) {
-                    uniqueWords.add(word);
+                    if (wordMap.containsKey(word))
+                        wordMap.put(word, wordMap.get(word) + 1);
+                    else
+                        wordMap.put(word, 1);
                 }
                 line = bufferedReader.readLine();
             }
-            System.out.println(uniqueWords);
+            System.out.println(wordMap.keySet());
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
